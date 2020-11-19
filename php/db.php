6fbf5adee4618a9da8989de9dbd998708e89941d
Sasha <lonagi22@gmail.com>
2020-11-19 15:01:13 +0200

    setup project

---
+++ php/db.php
@@ -0,0 +1,9 @@
+<?php
+$db = "fairytale";
+$l = "root";
+$p = "";
+
+
+$setup1 = R::setup('mysql:host=localhost;dbname='.$db, $l, $p);
+//R::addDatabase($db,'mysql:host=localhost;dbname='.$db, $l, $p);
+?>
