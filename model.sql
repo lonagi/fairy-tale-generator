84e5e3ba8322d898e335c8935031090df1d94b96
Sasha <lonagi22@gmail.com>
2020-11-21 00:37:19 +0200

    Update model.sql

--- model.sql
+++ model.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost
--- Время создания: Ноя 20 2020 г., 21:34
+-- Время создания: Ноя 20 2020 г., 22:33
 -- Версия сервера: 8.0.21
 -- Версия PHP: 7.4.9
 
@@ -24,46 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `characters`
---
-
-CREATE TABLE `characters` (
-  `id` int NOT NULL,
-  `fairy` int NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `gender` tinyint(1) NOT NULL,
-  `role` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `detail`
---
-
-CREATE TABLE `detail` (
-  `id` int NOT NULL,
-  `fairy` int NOT NULL,
-  `text` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `fairies`
---
-
-CREATE TABLE `fairies` (
-  `id` int NOT NULL,
-  `name` varchar(150) NOT NULL,
-  `text` text NOT NULL,
-  `autor` varchar(50) NOT NULL,
-  `language` varchar(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
 -- Структура таблицы `locations`
 --
 
@@ -73,53 +33,9 @@ CREATE TABLE `locations` (
   `name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `object`
---
-
-CREATE TABLE `object` (
-  `id` int NOT NULL,
-  `fairy` int NOT NULL,
-  `location` int NOT NULL,
-  `name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `season`
---
-
-CREATE TABLE `season` (
-  `id` int NOT NULL,
-  `fairy` int NOT NULL,
-  `detail` int NOT NULL,
-  `name` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 --
 -- Индексы сохранённых таблиц
 --
-
---
--- Индексы таблицы `characters`
---
-ALTER TABLE `characters`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `detail`
---
-ALTER TABLE `detail`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `fairies`
---
-ALTER TABLE `fairies`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `locations`
@@ -128,55 +44,13 @@ ALTER TABLE `locations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы `object`
---
-ALTER TABLE `object`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `season`
---
-ALTER TABLE `season`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT для сохранённых таблиц
 --
-
---
--- AUTO_INCREMENT для таблицы `characters`
---
-ALTER TABLE `characters`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `detail`
---
-ALTER TABLE `detail`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `fairies`
---
-ALTER TABLE `fairies`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `locations`
 --
 ALTER TABLE `locations`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `object`
---
-ALTER TABLE `object`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `season`
---
-ALTER TABLE `season`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
