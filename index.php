bfa5e28223330927740e146f16df30a171cd6cca
Sasha <lonagi22@gmail.com>
2020-11-20 22:11:12 +0200

    all cards

--- index.php
+++ index.php
@@ -10,8 +10,15 @@
 
     <div class="bg-main pt-5">
         <?php
-        include_once "templates/fairytales.phtml";
+            if(isset($_GET['fairy']))
+                include_once "templates/header.phtml";
+            else
+                include_once "templates/fairytales.phtml";
         ?>
     </div>
+
+    <?php
+        include_once "templates/content.phtml";
+    ?>
 </body>
 </html>
